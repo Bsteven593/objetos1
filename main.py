261b97fe88174424bfaf14d183efaf1afbb59657
unknown <Usuario@DESKTOP-7MUHP50>
2022-12-05 18:50:39 -0600

    ejercicios1

---
+++ main.py
@@ -0,0 +1,54 @@
+class miClase:
+    x=5
+    
+p1 = miClase()
+print(p1.x) 
+
+class persona:
+    nombre = str
+    edad = int
+    
+    def __init__(self,nombre,edad):
+        self.nombre = nombre
+        self.edad = edad 
+    def __str__(self):
+        return f"Su nombre es {self.nombre}, y su edad es {self.edad}." 
+       
+p2 = persona("Diego Alexandar", 29)
+
+print(p2)
+
+class Persona2:
+    nombre = str
+    edad = int
+    cedula = int
+  
+    def __init__(self, nombre, edad, cedula):
+        self.nombre = nombre
+        self.edad = edad
+        self.cedula = cedula
+    
+    def __str__(self):
+        return f"Hola mi nombre es  {self.nombre} y tengo {self.edad} años y mi numero de cédula es el: {self.cedula}"   
+        
+p3 = Persona2 ("Bryan", 19, 1726375767)
+p3.nombre = "Steven"
+p3.cedula = "1726375766"
+
+print(p3)
+
+class persona3:
+    nombre = str
+    edad = int
+    altura = int
+    
+    def __init__(self, nombre, edad, estatura):
+        self.nombre = nombre
+        self.edad = edad 
+        self.estatura= estatura
+    def __str__(self):
+        return f"Su nombre es {self.nombre} y tu edad es {self.edad} y tu estaruta es de: {self.estatura}"
+       
+p4 = persona3 ("Jose", 19, 1.67)
+
+print(p4)
---
+++ personas.py
@@ -0,0 +1,17 @@
+class persona:
+    nombre        = str
+    edad          = int
+    centroEstudio = str
+    def __init__(self,nombre,edad, centroEstudio):
+        self.nombre        = nombre 
+        self.edad          = edad
+        self.centroEstudio = centroEstudio
+        
+    def conversar (self,otra_persona):
+        return f'Hola {otra_persona.nombre} me llamo {self.nombre} y estudio en el {self.centroEstudio} mi edad es {self.edad} años'  
+    
+if __name__=="__main__" :
+    Persona1 = persona("Milton", 36, "YAVIRAC")
+    Persona2 = persona("Gaby", 31, "POLITECNICA NACIONAL") 
+    
+    print(Persona1.conversar(Persona2))  
